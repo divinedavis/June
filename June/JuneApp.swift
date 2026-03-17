bc5719992cd673636cafc85510dfaaec244f5f1b
Divine Davis <divinejdavis@gmail.com>
2026-03-17 17:27:35 -0400

    Initial Commit

---
+++ June/JuneApp.swift
@@ -0,0 +1,17 @@
+//
+//  JuneApp.swift
+//  June
+//
+//  Created by Divine Davis on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct JuneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
